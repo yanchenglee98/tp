618cd132766ad3953cdf3fed74d65fb599353955
lowjiefeng <lowjiefeng1998@gmail.com>
2020-10-10 17:00:42 +0800

    Improve the UI slightly in the reference

--- src/main/java/seedu/address/ui/ReferenceSection.java
+++ src/main/java/seedu/address/ui/ReferenceSection.java
@@ -1,11 +1,57 @@
 package seedu.address.ui;
 
-import javafx.scene.layout.Region;
+import java.util.Arrays;
+import java.util.List;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.Region;
+import seedu.address.storage.BlockSetting;
+
+/**
+ * Panel containing the reference to blocks present.
+ */
 public class ReferenceSection extends UiPart<Region> {
     private static final String FXML = "ReferenceSection.fxml";
 
+    @FXML
+    private ListView<BlockSetting> blockListView;
+
+    /**
+     * Creates a {@code ReferenceSection}.
+     */
     public ReferenceSection() {
         super(FXML);
+
+        ObservableList<BlockSetting> blocksReference = FXCollections.observableArrayList();
+        List<BlockSetting> blocks = Arrays.asList(
+                new BlockSetting("A"), new BlockSetting("B"));
+        blocksReference.addAll(blocks);
+
+        blockListView.setItems(blocksReference);
+        blockListView.setCellFactory(listView -> new BlockCardCell());
+    }
+
+    class BlockCardCell extends ListCell<BlockSetting> {
+        /**
+         * Overrides the default listCell view.
+         * @param item object to create
+         * @param empty boolean flag to check if the item is just a placeholder
+         * @see PersonListPanel
+         */
+        @Override
+        protected void updateItem(BlockSetting item, boolean empty) {
+            super.updateItem(item, empty);
+
+            if (empty || item == null) {
+                setGraphic(null);
+                setText(null);
+            } else {
+                setGraphic(new BlockSettingCard(item).getRoot());
+            }
+        }
     }
 }
