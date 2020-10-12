cf14f535150a91df56c7cdf7a10709caae816bda
yanchenglee98 <yanchenglee@gmail.com>
2020-10-12 16:26:33 +0800

    Rename export test methods

--- src/test/java/seedu/address/logic/commands/ExportCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ExportCommandTest.java
@@ -1,10 +1,10 @@
 package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.address.logic.commands.ExportCommand.MESSAGE_EMAIL_SUCCESS;
 import static seedu.address.logic.commands.ExportCommand.MESSAGE_PHONE_SUCCESS;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
-import static seedu.address.logic.commands.ExportCommand.MESSAGE_EMAIL_SUCCESS;
 
 import org.junit.jupiter.api.Test;
 
--- src/test/java/seedu/address/logic/parser/ExportCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ExportCommandParserTest.java
@@ -1,8 +1,8 @@
 package seedu.address.logic.parser;
 
+import static seedu.address.logic.commands.ExportCommand.MESSAGE_USAGE;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
-import static seedu.address.logic.commands.ExportCommand.MESSAGE_USAGE;
 
 import org.junit.jupiter.api.Test;
 
@@ -16,12 +16,12 @@ public class ExportCommandParserTest {
     private ExportCommandParser parser = new ExportCommandParser();
 
     @Test
-    public void parse_validArgs_email_returnsExportCommand() {
+    public void parse_validArgsEmail_returnsExportCommand() {
         assertParseSuccess(parser, "export email", new ExportCommand("email"));
     }
 
     @Test
-    public void parse_validArgs_phone_returnsExportCommand() {
+    public void parse_validArgsPhone_returnsExportCommand() {
         assertParseSuccess(parser, "export phone", new ExportCommand("phone"));
     }
 
