9405f156fc8dccc6fe8b1e699c95b03136089001
Aung Thuya Oo <thuyaoo200@gmail.com>
2020-10-23 06:27:34 +0800

    Apply SLAP to code and add logging

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -10,6 +10,7 @@ import java.util.function.Predicate;
 import seedu.address.commons.core.Messages;
 import seedu.address.model.Model;
 import seedu.address.model.person.Person;
+
 /**
  * Finds and lists all persons in address book whose name contains any of the argument keywords.
  * Keyword matching is case insensitive.
--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -10,19 +10,24 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import seedu.address.commons.core.LogsCenter;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.Block;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.RoomInBlockPredicate;
+import seedu.address.storage.JsonAddressBookStorage;
 
 /**
  * Parses input arguments and creates a new FindCommand object
  */
 public class FindCommandParser implements Parser<FindCommand> {
 
+    private static final Logger logger = LogsCenter.getLogger(JsonAddressBookStorage.class);
     /**
      * Parses the given {@code String} of arguments in the context of the FindCommand
      * and returns a FindCommand object for execution.
@@ -30,25 +35,18 @@ public class FindCommandParser implements Parser<FindCommand> {
      */
     public FindCommand parse(String args) throws ParseException {
         requireNonNull(args);
+        logger.log(Level.INFO, "going to start parsing find command");
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_BLOCK);
 
         List<Predicate<Person>> predicates = new ArrayList<>();
 
         if (argMultimap.getValue(PREFIX_NAME).isPresent()) {
-            String trimmedArgs = argMultimap.getValue(PREFIX_NAME).get().trim();
-            if (trimmedArgs.isEmpty()) {
-                throw new ParseException(FindCommand.MESSAGE_EMPTY_KEYWORD);
-            }
-            String[] nameKeywords = trimmedArgs.split("\\s+");
-            assert nameKeywords.length > 0 : "there should be some keywords";
-            predicates.add(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+            predicates.add(getNameKeywordPredicate(argMultimap.getValue(PREFIX_NAME).get()));
         }
 
         if (argMultimap.getValue(PREFIX_BLOCK).isPresent()) {
-            String block = argMultimap.getValue(PREFIX_BLOCK).get().trim();
-            Block searchedBlock = parseBlock(block);
-            predicates.add(new RoomInBlockPredicate(searchedBlock));
+            predicates.add(getBlockPredicate(argMultimap.getValue(PREFIX_BLOCK).get()));
         }
 
         if (predicates.isEmpty()) {
@@ -58,4 +56,21 @@ public class FindCommandParser implements Parser<FindCommand> {
         return new FindCommand(predicates);
     }
 
+    private Predicate<Person> getBlockPredicate(String block) throws ParseException {
+        logger.log(Level.INFO, "getting block predicate");
+        Block searchedBlock = parseBlock(block);
+        return new RoomInBlockPredicate(searchedBlock);
+    }
+
+    private Predicate<Person> getNameKeywordPredicate(String keywords) throws ParseException {
+        logger.log(Level.INFO, "adding name keywords to filter");
+        String trimmedKeywords = keywords.trim();
+        if (trimmedKeywords.isEmpty()) {
+            logger.log(Level.WARNING, "empty keywords for name");
+            throw new ParseException(FindCommand.MESSAGE_EMPTY_KEYWORD);
+        }
+        String[] nameKeywords = trimmedKeywords.split("\\s+");
+        assert nameKeywords.length > 0 : "there should be some keywords";
+        return new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords));
+    }
 }
