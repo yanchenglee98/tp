3af949e5950dac1fc0d56b4c31f2bf558b598e80
yanchenglee98 <yanchenglee@gmail.com>
2020-11-08 00:54:43 +0800

    Update javadoc

--- src/main/java/seedu/address/logic/parser/ClearEventAttendeesCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ClearEventAttendeesCommandParser.java
@@ -6,6 +6,10 @@ import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.ClearEventAttendeesCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
+/**
+ * Parses input arguments and creates a new ClearEventAttendees object
+ */
+
 public class ClearEventAttendeesCommandParser implements Parser<ClearEventAttendeesCommand> {
 
     /**
